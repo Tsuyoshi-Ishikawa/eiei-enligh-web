b3f69ad160bf82fbf13fffd810be4f7cafdc8e42
Tsuyoshi-Ishikawa <tsuyozo7@gmail.com>
2023-01-09 12:12:22 +0900

    modify utterance voice

--- src/utils/speech.ts
+++ src/utils/speech.ts
@@ -1,9 +1,16 @@
 export const speak = (text: string, callback: () => void) => {
+  const voices = speechSynthesis.getVoices();
+  const voice = voices.find((voice) => {
+    if (voice.name === 'Samantha' || voice.name === 'Victoria') return true;
+    return false;
+  });
+
   const utterance = new SpeechSynthesisUtterance();
   utterance.text = text;
   utterance.lang = 'en-US';
   utterance.rate = 1;
   utterance.pitch = 1.2;
+  if (voice) utterance.voice = voice;
   speechSynthesis.speak(utterance);
   utterance.onend = (event) => {
     callback();
