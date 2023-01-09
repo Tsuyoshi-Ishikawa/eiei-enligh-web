55269b8693a3907f0bea1a456b1a3f754f81425d
Tsuyoshi-Ishikawa <tsuyozo7@gmail.com>
2023-01-09 19:58:19 +0900

    modify setting voice with speechSynthesis

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,15 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head';
 import 'regenerator-runtime/runtime';
 import { Layout } from '@/components/templates';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // This command is necessary for setting voice.
+    // If here is not following command, we cannot set first speech voice.
+    const voices = speechSynthesis.getVoices();
+  }, []);
+
   return (
     <>
       <Head>
