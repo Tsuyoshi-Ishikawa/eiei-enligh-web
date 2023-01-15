288728648a270623a0ff92ecb7e0d4f84971f646
Tsuyoshi-Ishikawa <tsuyozo7@gmail.com>
2023-01-15 14:09:20 +0900

    install deepgram

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,10 @@
 import { Configuration, OpenAIApi } from 'openai';
+import { Deepgram } from '@deepgram/sdk';
 
 const configuration = new Configuration({
   apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
 });
 export const openai = new OpenAIApi(configuration);
+
+const DEEPGRAM_API_KEY = process.env.NEXT_PUBLIC_DEEPGRAM_API_KEY ?? '';
+export const deepgram = new Deepgram(DEEPGRAM_API_KEY);
